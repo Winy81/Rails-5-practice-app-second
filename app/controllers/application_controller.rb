5ad1d7a4f8f889a6512b35d2b8a8bfcd2d632009
iSmash <nemeth.richo@gmail.com>
2019-01-03 13:40:11 +0000

    basic setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def base_action
+    render html: "I am from the ApplicationController"
+  end
 end
