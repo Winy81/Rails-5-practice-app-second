9d70dd0c37cddd05d911a18268532d0b249f72bb
iSmash <nemeth.richo@gmail.com>
2019-01-13 17:29:41 +0000

    TDD test criterias for modell and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates(:name, presence: true)
+	validates :name, length: { minimum: 6 }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "length of name" do
+  	@user.name = "ez"
+    @user.name.length >= 5
+    assert_not @user.valid?
+  end
 end
