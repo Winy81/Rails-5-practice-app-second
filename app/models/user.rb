d6e2ea7d278ee0d084704bfc78840fc30d74e818
iSmash <nemeth.richo@gmail.com>
2019-01-14 16:08:35 +0000

    password coulum into teh user tbale and copule of validatros for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	before_save { email.downcase! }
 	validates(:name, presence: true)
 	validates(:name, length: { minimum: 5 })
     validates(:name, length: { maximum: 50 })
@@ -7,4 +8,6 @@ class User < ApplicationRecord
     validates(:email, length: { maximum: 255 })
     validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates(:email, uniqueness:{ case_sensitive: false })
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
