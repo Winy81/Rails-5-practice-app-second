658efce5e9a7a498a4b21502943e598d964c1e06
Adam Nemeth <nemeth.richo@gmail.com>
2019-01-14 07:23:34 +0000

    user.email validations and test criterias

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	validates(:name, presence: true)
-	validates :name, length: { minimum: 6 }
-
+	validates(:name, length: { minimum: 6 })
+  validates(:email, presence: true)
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,4 +20,9 @@ class UserTest < ActiveSupport::TestCase
     @user.name.length >= 5
     assert_not @user.valid?
   end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
