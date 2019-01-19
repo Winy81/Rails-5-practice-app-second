9af25a2633a878f900844eb3827782ab51fff159
iSmash <nemeth.richo@gmail.com>
2019-01-19 15:52:07 +0000

    user-login test amd flash for controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-    	
+
     else
+    	flash.now[:danger] = "Invalid email/password combintaion"
     	render 'new'
     end
   end
