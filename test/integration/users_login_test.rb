b1dd2c8f5a5d03114bd5467cc5d0c39edf47ca16
iSmash <nemeth.richo@gmail.com>
2019-01-19 15:56:44 +0000

    test explanation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+    get login_path #get requiest
+    assert_template 'sessions/new'  #check for session/new is prpoerly loading up
+    post login_path, params: { session: { email: "", password: "" } } #check the login with failed details
+    assert_template 'sessions/new'  #check for session/new is prpoerly loading up, as the login was failed
+    assert_not flash.empty? #ensure the flash is not empty
+    get root_path #get requiest for root_path
+    assert flash.empty? #ensure the flash is empty (passed away). witn "new" method on the flash
   end
 end
