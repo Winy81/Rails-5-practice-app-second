a633f9fc95e1204542ca8fd6643fb55f25fca4fe
iSmash <nemeth.richo@gmail.com>
2019-01-22 14:43:38 +0000

    User login helper and controller setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  include SessionsHelper
   def base_action
     render html: "I am from the ApplicationController"
   end
