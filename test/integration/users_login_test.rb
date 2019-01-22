385ef4cd652a8e6491091b6f5e616f6a0099225e
iSmash <nemeth.richo@gmail.com>
2019-01-22 19:01:47 +0000

    test for user login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+
   test "login with invalid information" do
     get login_path #get requiest
     assert_template 'sessions/new'  #check for session/new is prpoerly loading up
@@ -9,5 +15,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty? #ensure the flash is not empty
     get root_path #get requiest for root_path
     assert flash.empty? #ensure the flash is empty (passed away). witn "new" method on the flash
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect! #to check the right redirect target and
+    assert_template 'users/show' #to actually visit the target page
+    assert_select "a[href=?]", login_path, count: 0 #mean 0 login link on the page
+    assert_select "a[href=?]", logout_path #logout link is exist
+    assert_select "a[href=?]", user_path(@user) #user_path link exist
   end
 end
