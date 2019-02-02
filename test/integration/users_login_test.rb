3a4eb94de2798082c46a93172b1b965ef09b4bc1
iSmash <nemeth.richo@gmail.com>
2019-02-02 13:57:49 +0000

    more test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,4 +38,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0 #logout link not exist
     assert_select "a[href=?]", user_path(@user), count: 0 #user_path link not exist
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
