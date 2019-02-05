934f0488f6d3c838151257a2ae839ab279302411
iSmash <nemeth.richo@gmail.com>
2019-02-05 14:20:07 +0000

    store_location method and redirection calibrate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+      	store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -55,5 +56,5 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    
+
 end
