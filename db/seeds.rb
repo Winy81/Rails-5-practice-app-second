b57eb15c8e88dbc78bc9c48a390c0bb5db5b45f4
iSmash <nemeth.richo@gmail.com>
2019-02-08 16:05:28 +0000

    seeds.rb for users + layout correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+99.times do |var|
+	User.create!(name: "name"+ var.to_s,
+		     	 email: "example-#{var.to_s}"+"@barmi.com",
+		     	 password: "pass"+var.to_s,
+		     	 password_confirmation: "pass"+var.to_s	
+		         )
+end
