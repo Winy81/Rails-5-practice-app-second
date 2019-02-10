8e262da597b09a528df462cadbcf1a39ab51d57c
iSmash <nemeth.richo@gmail.com>
2019-02-10 11:51:43 +0000

    test for user delete action access

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
@@ -12,6 +13,18 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     User.paginate(page: 1, :per_page => 15).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      if user.admin == true
+      	assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
