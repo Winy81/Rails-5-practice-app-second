56f3a48602534aa9fab7a031d2b3eec854151305
iSmash <nemeth.richo@gmail.com>
2019-02-11 15:35:01 +0000

    seed.rb expansion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Admin user created for me
+
+User.create!(name: AdamAdmin
+			 email: adam@adam.com
+			 password: "pass"
+			 password_confirmation: "pass"
+			 admin: true
+			 activated: true
+			 activated_at: Time.zone.now
+	 		)
+
+#regular users for test
+
 65.times do |var|
 	User.create!(name: "name"+ var.to_s,
 		     	 email: "example-#{var.to_s}"+"@barmi.com",
 		     	 password: "pass"+var.to_s,
 		     	 password_confirmation: "pass"+var.to_s,
-		     	 admin: true	
+		     	 admin: false
+		     	 activated: true,
+                 activated_at: Time.zone.now	
 		         )
 end
